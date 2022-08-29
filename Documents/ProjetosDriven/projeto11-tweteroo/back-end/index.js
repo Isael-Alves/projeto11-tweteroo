9035ec6f125d4fcc169f84a5ca0869b6d1572dc5
Isael-Alves <isael.alves@tucurui.ufpa.br>
2022-08-29 13:55:31 -0300

    get tweets Okay

--- Documents/ProjetosDriven/projeto11-tweteroo/back-end/index.js
+++ Documents/ProjetosDriven/projeto11-tweteroo/back-end/index.js
@@ -11,23 +11,37 @@ const tweets = [];
 app.post("/sign-up", (req, res) => {
   const { username, avatar } = req.body;
 
-  if (!username || !avatar) {
+  if (!username || !avatar)
     return res.status(400).send("Preencha todos os campos!");
-  }
 
-  Users.push({ username, avatar });
+  Users.push({
+    username: username,
+    avatar: avatar,
+  });
 
   res.send("OK");
 });
 
 app.post("/tweets", (req, res) => {
-  tweets.push(req.body);
+  const { username, avatar } = req.body;
   res.send("OK");
 });
 
 app.get("/tweets", (req, res) => {
-  console.log(req);
-  res.send("tudo certo");
+  res.send(reverseTweets());
 });
+
+function reverseTweets() {
+  const Tweets = [...tweets].reverse();
+  if (tweets.length >= 0 && tweets.length <= 10) return Tweets;
+
+  const newArrayTweets = [];
+  let cond = 0;
+  while (cond < 10) {
+    newArrayTweets.push(Tweets[cond]);
+    cond++;
+  }
+  return newTweets;
+}
 
 app.listen(5000);
